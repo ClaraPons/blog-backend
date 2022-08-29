3ebd50d824a188c45410198a7ff975350677f8e0
ClaraPons <claragapons@gmail.com>
2022-08-29 22:52:36 +0200

    add validation

--- routes/article.js
+++ routes/article.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express('')
 const fs = require('fs')
 const moment = require('moment')
+const { body, validationResult } = require("express-validator")
 
 app.get('/', (req, res) => {
 
@@ -17,28 +18,40 @@ app.get('/', (req, res) => {
 
 })
 
-app.post('/', (req, res) => {
+app.post('/',
+    body('title').exists().withMessage('You have to add a title to your article'), 
+    body('content').isLength({ min: 5}).withMessage('Your article is too short'),
+    body('author').exists().withMessage('You have to add an author'),
+    (req, res) => {
+
+    const { errors } = validationResult(req)
+
     const article = {...req.body,
-    date: moment().format('MMMM Do YYYY, h:mm:ss')
-    }
-   
-
-    fs.readFile('./article.json', (err, data) => {
-        if(err){
-            console.log(err);
-            return
-        }else{
-            const articles = JSON.parse(data.toString());
-            articles.push(article)
-            res.json(articles)
-
-            fs.writeFile('./article.json', JSON.stringify(articles), err => {
-                console.log(err)
-              })
+        date: moment().format('MMMM Do YYYY, h:mm:ss')
         }
-    })
 
-    res.json('ok')
+
+        if(errors.length > 0) {
+            res.status(400).json(errors)
+        }else{
+            fs.readFile('./article.json', (err, data) => {
+                if(err){
+                    console.log(err);
+                    return
+                }else{
+                  
+                    const articles = JSON.parse(data.toString());
+                    articles.push(article)
+                    res.json(articles)
+        
+                    fs.writeFile('./article.json', JSON.stringify(articles), err => {
+                            console.log(err)
+                      })
+                }
+            })
+        }
+
+    // res.json('ok')
 
 })
 
